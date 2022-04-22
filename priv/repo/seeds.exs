bfdbf9ed430166efaeea968d2d9b19fb333df97e
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-04-22 18:33:42 -0300

    insert csvs into db

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,11 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     FoodTruckUs.Repo.insert!(%FoodTruckUs.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias FoodTruckUs.ConvertCsvToMap
+alias FoodTruckUs.FoodTruck
+alias FoodTruckUs.Repo
+
+"Mobile_Food_Facility_Permit.csv"
+|> ConvertCsvToMap.execute()
+|> Enum.each(fn food_truct ->
+  %FoodTruck{}
+  |> FoodTruck.changeset(food_truct)
+  |> Repo.insert()
+end)
