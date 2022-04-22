1b82a0c41494d9347d73971e0a28136d79451d4a
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-04-22 17:32:56 -0300

    change elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FoodTruckUs.MixProject do
     [
       app: :food_truck_us,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.13.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
