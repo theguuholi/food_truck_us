527db0fcadd95bb0d64d11e86a71a264894d51dc
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-04-25 11:31:47 -0300

    prepend new food truck

--- assets/js/hooks/foodTruckMap.js
+++ assets/js/hooks/foodTruckMap.js
@@ -3,8 +3,9 @@ import FoodTruckLeaflet from "./foodTruckMap/foodTruckLeaflet";
 const FoodTruckMap = {
     mounted() {
         selectPoint = JSON.parse(this.el.dataset.selectPoint)
-        this.map = new FoodTruckLeaflet(this.el, [selectPoint.latitude, selectPoint.longitude], event = {
-
+        this.map = new FoodTruckLeaflet(this.el, [selectPoint.latitude, selectPoint.longitude], event => {
+            const foodTruckId = event.target.options.foodTruckId;
+            this.pushEvent("marker-clicked", foodTruckId)
         })
         const foodTrucks = JSON.parse(this.el.dataset.foodTrucks);
         foodTrucks.forEach(foodTruck => {
@@ -13,6 +14,11 @@ const FoodTruckMap = {
 
         this.handleEvent("highlight-marker", e => {
             this.map.highlightMarker(e)
+        })
+
+        this.handleEvent("add-marker", e => {
+            this.map.addMarker(e);
+            this.map.highlightMarker(e);
         })
     }
 }
--- lib/food_truck_us_web/live/map/map_live.ex
+++ lib/food_truck_us_web/live/map/map_live.ex
@@ -3,6 +3,7 @@ defmodule FoodTruckUsWeb.MapLive do
   alias FoodTruckUs.FoodTrucks
 
   def mount(_params, _session, socket) do
+    if connected?(socket), do: FoodTrucks.subscribe()
     food_trucks = FoodTrucks.all([])
     socket = assign(socket, food_trucks: food_trucks, selected_food_truck: nil)
     {:ok, socket}
@@ -15,6 +16,21 @@ defmodule FoodTruckUsWeb.MapLive do
       socket
       |> assign(selected_food_truck: selected_food_truck)
       |> push_event("highlight-marker", selected_food_truck)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("marker-clicked", food_truck_id, socket) do
+    selected_food_truck = FoodTrucks.find_food_truck(socket.assigns.food_trucks, food_truck_id)
+    {:noreply, assign(socket, selected_food_truck: selected_food_truck)}
+  end
+
+  def handle_info({:new_food_truck, food_truck}, socket) do
+    socket =
+      socket
+      |> update(:food_trucks, &[food_truck | &1])
+      |> assign(selected_food_truck: food_truck)
+      |> push_event("add-marker", food_truck)
 
     {:noreply, socket}
   end
